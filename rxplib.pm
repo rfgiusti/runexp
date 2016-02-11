e290ac6a7646748ac1dec90b1928920887f0f927
Rafael Giusti <rfgiusti@gmail.com>
2016-02-11 17:16:56 -0200

    Read configurations from file ~/.runexp
    Configurations must be in the form key=value. Valid keys are:
    
    - qmanager: specify the address of the queue manager
    - port: specify port to listen (manager) or connect (runner)
    - hostname: specify the host name to be used by the runners in case the
      runner should be identified by something different than what
      "hostname -s" returns

---
+++ rxplib.pm
@@ -0,0 +1,37 @@
+package rxplib;
+
+use strict;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT = qw(readconfig);
+
+use File::HomeDir;
+
+my $home = File::HomeDir->my_home;
+
+sub readconfig {
+	return () unless -f "$home/.runexp";
+
+	my %validkeys = map {$_ => 1} qw(port qmanager hostname);
+
+	my %conf;
+	open FILE, "<$home/.runexp" or die "Error opening config file";
+	while (my $line = <FILE>) {
+		$line =~ s/^\s*//;
+		next if $line =~ /^#/;
+
+		chomp $line;
+		die "~/.runexp: bad entry: $line\n" unless $line =~ /^([A-Za-z0-9]+)\s*=(.+)/;
+		my $key = lc $1;
+		my $value = $2;
+		die "~/.runexp: bad key: $key\n" unless defined $validkeys{$key};
+		$conf{$key} = $value;
+	}
+	close FILE;
+
+	return %conf;
+}
+
+
+return 1;
